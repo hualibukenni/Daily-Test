d17a8acfa9cbdac840f8cc1b9d0053e4ff79a172
ehmatthes <eric@example.com>
2018-06-03 16:47:23 +0800

    2018060302commit

--- loop.py
+++ loop.py
@@ -1,10 +1,10 @@
-myList = [4,0,3,-13,5,8,31,36,-100]
+myList = [4,0,3,-13,36,300,-200]
 print ("before sort:")
 print(myList)
 
 lenList = len(myList)
 for i in range(0,lenList-1):
-    for j in range(i+1,lenList-1):
+    for j in range(i+1,lenList):
         if myList[i] > myList[j]:
             myList[i],myList[j] = myList[j],myList[i]
 print("after soft:")
