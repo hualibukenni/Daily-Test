08c0285c8bec3113a0c406b59be452fd4d0664d5
hualibukenni <1828686915@qq.com>
2018-06-12 13:50:32 +0800

    2018061201 commit

--- WebSpider/7.py
+++ WebSpider/7.py
@@ -2,5 +2,9 @@ import urllib.request
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 response = urllib.request.Request(url = 'http://1024.917rbb.info/pw/',headers = headers)
-print(urllib.request.urlopen(response).read())
+rs = urllib.request.urlopen(response).read()
+print(rs)
+# # print (rs.status)
+# print(rs.getheaders())
+# print(rs.getheader('Server'))
 # print(type(response))
