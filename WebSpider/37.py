31194da19ac026037fd52596bfd7f375f516cc1f
hualibukenni <1828686915@qq.com>
2018-06-20 16:53:33 +0800

    2018062003 commit

---
+++ WebSpider/37.py
@@ -0,0 +1,11 @@
+from lxml import etree
+text = '''
+<div>
+    <ul>
+        <li class="item-0"><a></a></li>
+        <li class="item-1"><a></a></li>
+        <li class="item-inactive"><a></a></li>
+        <li class="item-1"><a></a></li>
+    </ul>
+</div>
+'''
