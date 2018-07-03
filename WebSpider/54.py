aa145ae6918b6a2e38f649541728899d2cd7447d
ehmatthes <eric@example.com>
2018-07-03 20:54:32 +0800

    2018070301 commit

---
+++ WebSpider/54.py
@@ -0,0 +1,17 @@
+import requests
+from pyquery import PyQuery as pq
+
+url = 'https://www.zhihu.com/explore'
+headers = {
+  'User-Agent' : 'Mozila/5.0(Macintosh;Intel Mac OS X 10_12_3)AppleWebkit/537.36(KHTML,like Gecko) chrome/58.0.3029.110 Safari/537.36'
+}
+html = requests.get(url,headers=headers).text
+doc = pq(html)
+items = doc('.explore-tab .feed-item').items()
+for item in items:
+    question = item.find('h2').text()
+author = item.find('.author-link-line').text()
+answer = pq(item.find('.content').html()).text()
+file = open('explore.txt','a',encoding='utf-8')
+file.write('\n'.join([question,author,answer]))
+file.close()
