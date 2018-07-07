814ed71925ea97226b10cfdb5173a081fb82ecee
hualibukenni <1828686915@qq.com>
2018-07-07 21:58:36 +0800

    20180707commit02

---
+++ WebSpider/66.py
@@ -0,0 +1,20 @@
+import pymysql
+data = {
+    'id':'20120002',
+    'name':'Bob',
+    'age':20
+}
+db = pymysql.connect(host='localhost',user='root',password='lhyaiwh',port=3306,db='spiders')
+cursor = db.cursor()
+table = 'students'
+keys = ', '.join(data.keys())
+values = ', '.join(['%s']*len(data))
+sql =   'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table,keys=keys,values=values)
+try:
+    if cursor.execute(sql,tuple(data.values())):
+        print('Succesful')
+        db.commit()
+except:
+    print('Failed')
+    db.rollback()
+db.close()
