76319081eb686c242964e3a93dffe3e973d85ab2
hualibukenni <1828686915@qq.com>
2018-07-22 20:25:01 +0800

    2018072202 commit

--- WebSpider/91.py
+++ WebSpider/91.py
@@ -1,1 +1,14 @@
-import scrapy
+import time
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+browser.get('https://www.baidu.com')
+time.sleep(2)
+browser.get('https://www.taobao.com')
+time.sleep(2)
+browser.get('https://www.zhihu.com')
+browser.back()
+time.sleep(5)
+browser.forward()
+
